60cdf4b9c7cb493d80a7b52d6268b3601e306f19
Espen Emil Høines <espenemil@espens-macbook-pro-2.local>
2019-10-14 13:35:41 +0200

    Initial Commit

---
+++ pg5600-exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pg5600-exam
+//
+//  Created by Espen Emil Høines on 14.10.2019.
+//  Copyright © 2019 Espen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
